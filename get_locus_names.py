05dc9819a1619b4c1a8a30e41bcb96f9d4d8724e
baizm <baizm@umich.edu>
2016-05-25 14:07:01 -0400

    Update get_locus_names.py

--- get_locus_names.py
+++ get_locus_names.py
@@ -6,15 +6,15 @@ o='/scratch/lsa_flux/baizm/bgc/loci.csv'
 f=open(f, 'r')
 o=open(o, 'w')
 
-n_loci=0
+n_loci=-1
 
 for i in f:
 	if '#' not in i:
 		split=re.split('\t', i.strip())
 		n_loci += 1
-		l=','.join(map(str,split[0:2]))
-		o.write('locus '+str(n_loci)+','+l+'\n')
+		l=' '.join(map(str,split[0:2]))
+		o.write('loc_'+str(n_loci)+' '+l+'\n')
 
-#to make csv with bgc locus numbers & names corresponding from vcf file
+#to make file with bgc locus numbers & names corresponding from vcf file
 #e.g.:
-#locus 1,flattened_line_0,1234
+#loc_0 flattened_line_0 1234
